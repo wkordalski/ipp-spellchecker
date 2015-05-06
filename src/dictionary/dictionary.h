b868c807ee22e42c4a76e5413e524133fd1aa0a6
Tomasz Kociumaka <kociumaka@mimuw.edu.pl>
2015-05-06 15:58:34 +0200

    doxygen repaired

--- src/dictionary/dictionary.h
+++ src/dictionary/dictionary.h
@@ -1,5 +1,5 @@
 /** @file
-    Interfejs biblioteki obsługującej słownik
+    Interfejs biblioteki obsługującej słownik.
     @author Jakub Pawlewicz <pan@mimuw.edu.pl>
     @copyright Uniwerstet Warszawski
     @date 2015-05-06
@@ -12,20 +12,37 @@
 
 
 /**
-  @brief Struktura przechowująca słownik.
+  Struktura przechowująca słownik.
   */
 struct dictionary;
 
+
+/**
+  Inicjalizacja słownika.
+  @param[in,out] dictionary Słownik.
+  */
 void dictionary_init(struct dictionary*);
 
-void dictionary_done(struct dictionary*);
+
 /**
-  @brief Wstawia podane słowo do słownika.
-  @param[in,out] dictionary Słownik
+  Destrukcja słownika.
+  @param[in,out] dictionary Słownik.
+  */
+void dictionary_done(struct dictionary*);
+
+/**
+  Wstawia podane słowo do słownika.
+  @param[in,out] dictionary Słownik.
   @param[in] word Słowo, które należy wstawić do słownik.
   */
-
 void dictionary_insert(struct dictionary*, const wchar_t*);
+
+
+/**
+  Zwraca słowo znajdujące się w słowniku.
+  @param[in,out] dictionary Słownik.
+  @return Słowo znajdujące się w słowniku
+  */
 
 const wchar_t* dictionary_getword(struct dictionary *);
 
