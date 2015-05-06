57fd0cbb3f2af04fa90608f7ac6dda041c62e160
Jakub Pawlewicz <pan@mimuw.edu.pl>
2015-05-06 15:43:48 +0200

    Rozbudowa prostego słownika

---
+++ src/dictionary/dictionary.c
@@ -0,0 +1,34 @@
+#include "dictionary.h"
+
+struct dictionary
+{
+    wchar_t *word;
+};
+
+void dictionary_init(struct dictionary *dict)
+{
+    dict->word = NULL;
+}
+
+static void dictionary_free(struct dictionary *dict)
+{
+    if (dict->word)
+        free(dict->word);
+}
+
+void dictionary_done(struct dictionary *dict)
+{
+    dictionary_free(dict);
+    dict->word = NULL;
+}
+
+void dictionary_insert(struct dictionary *dict, const wchar_t *word)
+{
+    dictionary_free(dict);
+    dict->word = wcsdup(word);
+}
+
+const wchar_t* dictionary_getword(struct dictionary *)
+{
+    return dict->word;
+}
--- src/dictionary/dictionary.h
+++ src/dictionary/dictionary.h
@@ -16,11 +16,17 @@
   */
 struct dictionary;
 
+void dictionary_init(struct dictionary*);
+
+void dictionary_done(struct dictionary*);
 /**
   @brief Wstawia podane słowo do słownika.
   @param[in,out] dictionary Słownik
   @param[in] word Słowo, które należy wstawić do słownik.
   */
-int dictionary_insert(struct dictionary*, const wchar_t* word);
+
+void dictionary_insert(struct dictionary*, const wchar_t*);
+
+const wchar_t* dictionary_getword(struct dictionary *);
 
 #endif /* __DICTIONARY_H__ */
