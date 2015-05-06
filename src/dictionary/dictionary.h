00a1f0cef2d34634502cefb41bb1cda2e3808b9b
Tomasz Kociumaka <kociumaka@mimuw.edu.pl>
2015-05-06 15:40:52 +0200

    Doxygen: początki

--- src/dictionary/dictionary.h
+++ src/dictionary/dictionary.h
@@ -1,10 +1,26 @@
+/** @file
+    Interfejs biblioteki obsługującej słownik
+    @author Jakub Pawlewicz <pan@mimuw.edu.pl>
+    @copyright Uniwerstet Warszawski
+    @date 2015-05-06
+ */
+
 #ifndef __DICTIONARY_H__
 #define __DICTIONARY_H__
 
 #include <wchar.h>
 
+
+/**
+  @brief Struktura przechowująca słownik.
+  */
 struct dictionary;
 
-int dictionary_insert(struct dictionary*, const wchar_t*);
+/**
+  @brief Wstawia podane słowo do słownika.
+  @param[in,out] dictionary Słownik
+  @param[in] word Słowo, które należy wstawić do słownik.
+  */
+int dictionary_insert(struct dictionary*, const wchar_t* word);
 
 #endif /* __DICTIONARY_H__ */
