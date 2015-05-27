7b3d495552f457e3f0c2ab97523d9b4e28aaee06
Jakub Pawlewicz <pan@mimuw.edu.pl>
2015-05-27 18:27:49 +0200

    Zmieniona deklaracja funkcji dictionary_lang_list()

--- src/dictionary/dictionary.h
+++ src/dictionary/dictionary.h
@@ -7,7 +7,7 @@
     @ingroup dictionary
     @author Jakub Pawlewicz <pan@mimuw.edu.pl>
     @copyright Uniwersytet Warszawski
-    @date 2015-05-10
+    @date 2015-05-27
  */
 
 #ifndef __DICTIONARY_H__
@@ -98,11 +98,20 @@ void dictionary_hints(const struct dictionary *dict, const wchar_t* word,
   Zwraca nazwy języków, dla których dostępne są słowniki.
   Powinny to być nazwy lokali bez kodowania. np.
   Przykładowe nazwy pl_PL, albo en_US.
-  @param[out] size Liczba dostępnych języków.
-  @return Tablica nazw języków.
-  Użytkownik jest odpowiedzialny za zwolnienie tej tablicy.
+  Reprezentacja listy języków jest podobna do
+  list łańcuchów znakowych [argz w glibc'u](http://www.gnu.org/software/libc/manual/html_mono/libc.html#Argz-and-Envz-Vectors).
+  `*list` jest wskaźnikiem na początek bufora,
+  który ma długość `*list_len`.
+  W buforze znajdują się łańcuchy znakowe
+  jeden po drugim pooddzielane znakiem '\0'.
+  Jeśli lista jest niepusta cały bufor też się kończy znakiem '\0'.
+  Użytkownik jest odpowiedzialny za zwolnienie tej listy,
+  w tym celu wystarczy wywołać `free(*list)`.
+  @param[out] list Lista dostępnych języków.
+  @param[out] list_len Długość bufora z listą dostępnych języków.
+  @return <0 jeśli operacja się nie powiodła, 0 w p.p.
   */
-const char * * dictionary_lang_list(int *size);
+int dictionary_lang_list(char **list, size_t *list_len);
 
 
 /**
